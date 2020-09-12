ec9e2017d297359b2835ce412d4c129558d6d1ad
Prieyudha Akadita S <akaditasustono@gmail.com>
2020-09-12 18:43:02 +0700

    Updates

---
+++ precache-manifest.c92018b5cd0eca28407293420d39225e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b44ae5f5acbee357e961d85f6dfcf6",
+    "url": "/react_js_component_basic/index.html"
+  },
+  {
+    "revision": "091a745b90effbca94d0",
+    "url": "/react_js_component_basic/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0577291667503162fb69",
+    "url": "/react_js_component_basic/static/js/2.bd06ba5b.chunk.js"
+  },
+  {
+    "revision": "f80cd9fcede38bac8dac7ae5de1a5e40",
+    "url": "/react_js_component_basic/static/js/2.bd06ba5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "091a745b90effbca94d0",
+    "url": "/react_js_component_basic/static/js/main.734332a3.chunk.js"
+  },
+  {
+    "revision": "f714d736455fd95e06ef",
+    "url": "/react_js_component_basic/static/js/runtime-main.babda745.js"
+  }
+]);
